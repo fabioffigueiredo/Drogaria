02ad37b444638760cc3f879614df640d78e7487b
Fabio Figueiredo <fabioinformacao@gmail.com>
2017-12-05 23:11:03 -0200

    editar cidadeDAOTest & ProdutoDAO
    Inclusão do ProdutoDAO e o editar no cidadeDAOTest

---
+++ src/main/java/br/com/fabio/drogaria/dao/ProdutoDAO.java
@@ -0,0 +1,8 @@
+package br.com.fabio.drogaria.dao;
+
+import br.com.fabio.drogaria.domain.Produto;
+
+public class ProdutoDAO extends GenericDAO<Produto>{
+
+	
+}
--- src/test/java/br/com/fabio/drogaria/dao/CidadeDAOTest.java
+++ src/test/java/br/com/fabio/drogaria/dao/CidadeDAOTest.java
@@ -48,12 +48,45 @@ public class CidadeDAOTest {
 		System.out.println("Nome do Estado:" + cidade.getEstado().getNome());	
 	}
 	@Test
+	@Ignore
 	public void excluir() {
 		Long codigo = 1L;
 		CidadeDAO cidadeDAO = new CidadeDAO();
 		Cidade cidade = cidadeDAO.buscar(codigo);
 		cidadeDAO.excLuir(cidade);
 		System.out.println("Cidade removida");
+		System.out.println("Código da Cidade:" + cidade.getCodigo());
+		System.out.println("Nome da Cidade:" + cidade.getNome());
+		System.out.println("Código do Estado:" + cidade.getEstado().getCodigo());
+		System.out.println("Sigla do Estado:" + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado:" + cidade.getEstado().getNome());	
+	}
+	@Test
+	public void editar() {
+		Long codigoCidade = 2L;
+		long codigoEstado = 6L;
+		
+		EstadoDAO estadoDAO = new EstadoDAO();
+		Estado estado = estadoDAO.buscar(codigoEstado);
+		
+		System.out.println("Código do Estado:" + estado.getCodigo());
+		System.out.println("Sigla do Estado:" + estado.getSigla());
+		System.out.println("Nome do Estado:" + estado.getNome());	
+		
+		CidadeDAO cidadeDAO = new CidadeDAO();
+		Cidade cidade = cidadeDAO.buscar(codigoCidade);
+		System.out.println("Cidade a ser editada");
+		System.out.println("Código da Cidade:" + cidade.getCodigo());
+		System.out.println("Nome da Cidade:" + cidade.getNome());
+		System.out.println("Código do Estado:" + cidade.getEstado().getCodigo());
+		System.out.println("Sigla do Estado:" + cidade.getEstado().getSigla());
+		System.out.println("Nome do Estado:" + cidade.getEstado().getNome());	
+		
+		cidade.setNome("Nova Iguaçu");
+		cidade.setEstado(estado);
+		cidadeDAO.editar(cidade);
+		
+		System.out.println("Cidade editada");
 		System.out.println("Código da Cidade:" + cidade.getCodigo());
 		System.out.println("Nome da Cidade:" + cidade.getNome());
 		System.out.println("Código do Estado:" + cidade.getEstado().getCodigo());

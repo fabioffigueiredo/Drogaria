cdf050b1c6080aa31fb678dfc8ab13c61d4835a8
Fabio Figueiredo <fabioinformacao@gmail.com>
2017-12-09 18:25:52 -0200

    ClienteDAOTest

--- src/test/java/br/com/fabio/drogaria/dao/ClienteDAOTest.java
+++ src/test/java/br/com/fabio/drogaria/dao/ClienteDAOTest.java
@@ -1,16 +1,21 @@
 package br.com.fabio.drogaria.dao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.junit.Test;
 
 import br.com.fabio.drogaria.domain.Cliente;
 import br.com.fabio.drogaria.domain.Pessoa;
 
 public class ClienteDAOTest {
-  public void salvar() {
+  @Test
+	public void salvar() throws ParseException {
 	  PessoaDAO pessoaDAO = new PessoaDAO();
-	  Pessoa pessoa = pessoaDAO.buscar(1L);
+	  Pessoa pessoa = pessoaDAO.buscar(2L);
 	  Cliente cliente = new Cliente();
-	  cliente.setDataCadastro(new Date());
+	  cliente.setDataCadastro(new SimpleDateFormat("dd/MM/yyyy").parse("09/06/2017"));
 	  cliente.setLiberado(true);
 	  cliente.setPessoa(pessoa);
 	  
